7e0173353a049b7984d4d3383ad5ca76ea4bbe07
Abhinav Trivedi <abhinavtrivedii@outlook.com>
2026-07-05 13:22:00 +0530

    Initial commit for spring security -jwt impl

--- src/main/java/com/parctice/spring/security/entity/Users.java
+++ src/main/java/com/parctice/spring/security/entity/Users.java
@@ -1,12 +1,10 @@
 package com.parctice.spring.security.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class Users {
 
     @Id
